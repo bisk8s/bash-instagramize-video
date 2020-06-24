35637a1ee727c094375a7bc6485236c96638443b
Guilherme Lucena <lucena_guilherme@yahoo.com>
2020-06-24 16:48:13 -0300

    better filter image support

--- instagramize.sh
+++ instagramize.sh
@@ -2,15 +2,12 @@
 
 clear
 
-VIDEO_SIZE=600
-FILE_PATH=$1
+INPUT_PATH=$1
 FILE_NAME=$(basename -- "$1"| cut -f 1 -d '.')
-OUTPUT_FILE="${FILE_NAME}_instagramized.mp4"
+FILE_EXT=$(basename -- "$1"| cut -f 2 -d '.')
+OUTPUT_FILE="${FILE_NAME}_instagramized.${FILE_EXT}"
 
-ffmpeg -i $1 \
-    -c:v libx264 -crf 23 \
-    -filter_complex "[0:v]split=2[blur][vid];[blur]scale=$VIDEO_SIZE:$VIDEO_SIZE:force_original_aspect_ratio=increase,crop=$VIDEO_SIZE:$VIDEO_SIZE,boxblur=luma_radius=min(h\,w)/20:luma_power=1:chroma_radius=min(cw\,ch)/20:chroma_power=1[bg];[vid]scale=$VIDEO_SIZE:$VIDEO_SIZE:force_original_aspect_ratio=decrease[ov];[bg][ov]overlay=(W-w)/2:(H-h)/2" \
-    -profile:v baseline -level 3.0 -pix_fmt yuv420p -preset faster -tune fastdecode \
-    -c:a aac -ac 2 -b:a 128k \
-    -movflags faststart \
-    $OUTPUT_FILE -y
+ffmpeg -i $INPUT_PATH \
+       -filter_complex_script squareblured.filter\
+       -y \
+       $OUTPUT_FILE
--- instagramize_all.sh
+++ instagramize_all.sh
@@ -2,9 +2,19 @@
 
 clear
 find . -maxdepth 1 -name '*.mp4'   -type f -exec bash -c './instagramize.sh $1' _ {} \;
+find . -maxdepth 1 -name '*.jpg'   -type f -exec bash -c './instagramize.sh $1' _ {} \;
+find . -maxdepth 1 -name '*.png'   -type f -exec bash -c './instagramize.sh $1' _ {} \;
+find . -maxdepth 1 -name '*.jpeg'   -type f -exec bash -c './instagramize.sh $1' _ {} \;
 
 mkdir instagramized
 mkdir originals
 
 mv *_instagramized.mp4 ./instagramized
+mv *_instagramized.jpg ./instagramized
+mv *_instagramized.png ./instagramized
+mv *_instagramized.jpeg ./instagramized
+
 mv *.mp4 ./originals 
+mv *.jpg ./originals 
+mv *.png ./originals 
+mv *.jpeg ./originals 
